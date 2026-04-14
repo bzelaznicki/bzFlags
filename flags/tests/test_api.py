a587d249b803a06c1a380406c3f51e040f6a665a
bzelaznicki <bartosz@zelaznicki.com>
2026-04-15 00:14:06 +0200

    added non-existing flag response test

--- flags/tests/test_api.py
+++ flags/tests/test_api.py
@@ -1,10 +1,30 @@
+import pytest
 from rest_framework.test import APIClient
+from model_bakery import baker 
 
+@pytest.mark.django_db
 def test_api_should_return_401_on_invalid_api_key():
     client = APIClient()
-    res = client.post('/api/evaluate', json={
+    res = client.post('/api/evaluate', data={
         'user_identifier': 'user_123',
-        'flag_key': 'bzflags-test'
-    });
+        'flag_keys': ['bzflags-test']
+    }, 
+                      format='json')
 
     assert res.status_code == 401
+
+@pytest.mark.django_db
+def test_api_should_not_return_non_existing_keys():
+    project = baker.make('flags.Project')
+
+    client = APIClient()
+    res = client.post('/api/evaluate', data={
+        'user_identifier': 'user_123',
+        'flag_keys': ['does-not-exist']
+    },
+                      headers={'X-Api-Key': project.api_key},
+
+                      format='json')
+
+    assert res.status_code == 200
+    assert res.data == {}

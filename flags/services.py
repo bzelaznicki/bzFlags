9cea9a98109e164dd8e43ba08654eca70d9af30b
bzelaznicki <bartosz@zelaznicki.com>
2026-04-14 20:58:25 +0200

    rollouts implementation

--- flags/services.py
+++ flags/services.py
@@ -1,3 +1,5 @@
+from hashlib import sha256
+
 def evaluate_flag(
     flag_enabled: bool,
     rollout_percentage: int,
@@ -7,3 +9,12 @@ def evaluate_flag(
 ) -> bool:
     if not flag_enabled:
         return False
+    if rollout_percentage == 0:
+        return False
+    
+    hashed_string = sha256(bytes(user_identifier + flag_key, "utf-8")).hexdigest()
+    score = int("0x" + hashed_string, 0) 
+    if score % 101 > rollout_percentage:
+        return False
+
+    return True
--- flags/tests/test_services.py
+++ flags/tests/test_services.py
@@ -20,3 +20,60 @@ def test_fully_enabled_flag_returns_true():
         overrides={}
     )
     assert result is True
+
+
+def test_zero_percent_rollout_returns_false():
+    result = evaluate_flag(
+        flag_enabled=True,
+        rollout_percentage=0,
+        user_identifier="user_123",
+        flag_key="bzflags-test",
+        overrides={}
+    )
+    assert result is False
+
+
+def test_rollout_is_deterministic():
+    user_identifier = "user_123"
+    flag_key = "bzflags-test"
+
+    result_1 = evaluate_flag(
+        flag_enabled=True,
+        rollout_percentage=50,
+        user_identifier=user_identifier,
+        flag_key=flag_key,
+        overrides={}
+    )
+
+    result_2 = evaluate_flag(
+        flag_enabled=True,
+        rollout_percentage=50,
+        user_identifier=user_identifier,
+        flag_key=flag_key,
+        overrides={}
+    ) 
+
+    assert result_1 == result_2 
+
+
+def test_sticky_rollouts():
+    user_identifier = "user_123"
+    flag_key = "bzflags-test"
+
+    result_1 = evaluate_flag(
+        flag_enabled=True,
+        rollout_percentage=90,
+        user_identifier=user_identifier,
+        flag_key=flag_key,
+        overrides={}
+    )
+
+    result_2 = evaluate_flag(
+        flag_enabled=True,
+        rollout_percentage=10,
+        user_identifier=user_identifier,
+        flag_key=flag_key,
+        overrides={}
+    )
+
+    assert not result_2 or result_1

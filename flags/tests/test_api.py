95c8467453478ba2e9e2d94351c9444ae91229a1
bzelaznicki <bartosz@zelaznicki.com>
2026-04-15 19:24:12 +0200

    existing flag test

--- flags/tests/test_api.py
+++ flags/tests/test_api.py
@@ -28,3 +28,17 @@ def test_api_should_not_return_non_existing_keys():
 
     assert res.status_code == 200
     assert res.data == {}
+
+@pytest.mark.django_db
+def test_api_should_return_existing_flag():
+    project = baker.make('flags.Project')
+    baker.make('flags.Flag', project=project, key='bzflags-test')
+
+    client = APIClient()
+    res = client.post('/api/evaluate', data={
+        'user_identifier': 'user_123',
+        'flag_keys': ['bzflags-test']
+    }, headers={'X-Api-Key': project.api_key}, format='json')
+
+    assert res.status_code == 200
+    assert 'bzflags-test' in res.data

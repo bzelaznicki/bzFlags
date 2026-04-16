91288dfc0b36fb70e9d1bfe7ad771f5839c91fd3
bzelaznicki <bartosz@zelaznicki.com>
2026-04-16 20:42:37 +0200

    more tests

--- flags/tests/test_api_projects.py
+++ flags/tests/test_api_projects.py
@@ -1,6 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from model_bakery import baker
+from django.conf import settings
 
 
 @pytest.mark.django_db
@@ -8,8 +9,28 @@ def test_api_create_project():
     client = APIClient()
     res = client.post('/api/projects', data={
         'name': 'testproject'
-    })
+    }, 
+                      headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY}
+                      , format='json')
 
     assert res.status_code == 201
     assert res.data['name'] == 'testproject'
     assert res.data['api_key']
+
+@pytest.mark.django_db
+def test_api_invalid_admin_key():
+    client = APIClient()
+    res = client.post(path='/api/projects', data={'name': 'testproject'}, 
+                      headers={'X-Admin-Key': 'i-dont-know-what-a-key-is'},
+                      format='json')
+
+    assert res.status_code == 401
+    
+@pytest.mark.django_db
+def test_api_admin_key_missing():
+    client = APIClient()
+    res = client.post(path='/api/projects', data={'name': 'testproject'}, 
+                      format='json')
+
+    assert res.status_code == 401
+

00de4d9e9e47d4913958e335b7f9ff606f50d141
bzelaznicki <bartosz@zelaznicki.com>
2026-04-17 22:36:00 +0200

    added 8 tests for get all, single and delete

--- flags/tests/test_api_projects.py
+++ flags/tests/test_api_projects.py
@@ -2,6 +2,7 @@ import pytest
 from rest_framework.test import APIClient
 from model_bakery import baker
 from django.conf import settings
+import uuid
 
 
 @pytest.mark.django_db
@@ -46,3 +47,144 @@ def test_api_duplicate_project_name():
                       format='json')
 
     assert res.status_code == 409
+
+
+@pytest.mark.django_db
+def test_api_get_list_of_projects():
+    for i in range(5):
+        baker.make('flags.Project')
+
+    client = APIClient()
+    res = client.get(
+                    path='/api/projects',
+                    headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY},
+                    )
+
+    assert res.status_code == 200
+    assert len(res.data) == 5
+
+@pytest.mark.django_db
+def test_api_empty_list_of_projects():
+    client = APIClient()
+    res = client.get('/api/projects',
+                     headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY},
+                     )
+    assert res.status_code == 200
+    assert res.data == []
+
+@pytest.mark.django_db
+def test_api_get_projects_invalid_admin_key():
+    client = APIClient()
+    res = client.get('/api/projects', 
+                     headers={'X-Admin-Key': 'this-is-not-my-admin-key'}
+                     )
+
+    assert res.status_code == 401
+
+@pytest.mark.django_db
+def test_api_get_projects_missing_admin_key():
+    client = APIClient()
+    res = client.get('/api/projects')
+
+    assert res.status_code == 401
+
+@pytest.mark.django_db
+def test_api_get_single_project():
+    project = baker.make('flags.Project')
+    client = APIClient()
+
+    res = client.get(f'/api/projects/{project.id}', 
+                     headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+    assert res.status_code == 200
+    assert project.name == res.data['name']
+    assert project.api_key == res.data['api_key']
+    assert project.id == res.data['id']
+
+
+
+@pytest.mark.django_db
+def test_api_project_not_found():
+    client = APIClient()
+
+    res = client.get(f'/api/projects/{uuid.uuid4()}',
+                     headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+
+    assert res.status_code == 404
+
+@pytest.mark.django_db
+def test_api_get_single_project_invalid_admin_key():
+    project = baker.make('flags.Project')
+    client = APIClient()
+
+    res = client.get(f'/api/projects/{project.id}', 
+                     headers={'X-Admin-Key': 'invalid-key'})
+
+    assert res.status_code == 401
+
+
+
+@pytest.mark.django_db
+def test_api_get_single_project_missing_admin_key():
+    project = baker.make('flags.Project')
+    client = APIClient()
+
+    res = client.get(f'/api/projects/{project.id}')
+
+    assert res.status_code == 401
+
+
+@pytest.mark.django_db
+def test_api_delete_project():
+    project = baker.make('flags.Project')
+    client = APIClient()
+
+    res = client.delete(path=f'/api/projects/{project.id}',
+                        headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+    assert res.status_code == 204
+
+@pytest.mark.django_db
+def test_api_delete_project_not_found():
+    client = APIClient()
+
+    res = client.delete(path=f'/api/projects/{uuid.uuid4()}',
+                        headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+    assert res.status_code == 404
+    
+@pytest.mark.django_db
+def test_api_delete_project_invalid_admin_key():
+    project = baker.make('flags.Project')
+
+    client = APIClient()
+
+    res = client.delete(path=f'/api/projects/{project.id}',
+                        headers={'X-Admin-Key': 'this-key-is-invalid'})
+
+    assert res.status_code == 401
+
+@pytest.mark.django_db
+def test_api_delete_project_missing_admin_key():
+    project = baker.make('flags.Project')
+    client = APIClient()
+
+    res = client.delete(path=f'/api/projects/{project.id}')
+
+    assert res.status_code == 401
+
+@pytest.mark.django_db
+def test_api_delete_project_does_not_reappear():
+    project = baker.make('flags.Project')
+
+    client = APIClient()
+
+    client.delete(path=f'/api/projects/{project.id}',
+                  headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+    res = client.get(f'/api/projects/{project.id}',
+                        headers={'X-Admin-Key': settings.ADMIN_SECRET_KEY})
+
+    assert res.status_code == 404
+

37f85a8528ecdde69e21f9ef00178bb4eb1afb1f
bzelaznicki <bartosz@zelaznicki.com>
2026-04-18 00:00:11 +0200

    added secrets

--- flags/authentication.py
+++ flags/authentication.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
+import secrets
 
 
 class AdminKeyAuthentication(BaseAuthentication):
